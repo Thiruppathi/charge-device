2c09178f58e936f004da0ff59fb70163eb0d6305
Thiru <thiru.androider@gmail.com>
2016-03-12 14:11:56 +0000

    Amber. Danger lights.

--- charge-device.js
+++ charge-device.js
@@ -4,7 +4,6 @@ var Firebase = require("firebase");
 var assetsRef = new Firebase("https://myjarvis.firebaseio.com/assets/");
 
 var isOn = false;
-var isDockedIn = false;
 var assetId = 'EE1';
 var board = new five.Board();
 
@@ -12,26 +11,37 @@ board.once('ready', function() {
 
   var ee1_led13 = new five.Led(13);
   var alertLED = new five.Led(9);
-
-  assetsRef.child(assetId).child('batteryStatus').on("value", function(snapshot) {
-    isDockedIn = snapshot.child('isDocked').val();
-		console.log('isDockedIn ' + isDockedIn);
-		isDockedIn ? alertLED.off() : alertLED.on();
-	});
+  var amberLED = new five.Led(5);
 
   assetsRef.child(assetId).child('batteryStatus').on("value", function(snapshot) {
     var ee1BatteryLevel = snapshot.child('level').val();
     var isDocked = snapshot.child('isDocked').val();
-    if (isDocked && ee1BatteryLevel === 100) {
+    var isDanger = snapshot.child('isDanger').val();
+    var isAmber = snapshot.child('isAmber').val();
+
+    console.log('isDanger', isDanger);
+    if (isDocked && ee1BatteryLevel === 50) {
       isOn = false;
-    } else if (isDocked && ee1BatteryLevel !== 100) {
+    } else if (isDocked && ee1BatteryLevel !== 50) {
       isOn = true;
     } else if (!isDocked) {
       isOn = false;
     }
-
     isOn ? ee1_led13.on() : ee1_led13.off();
-    console.log('At End ' + isOn);
+    if (isDanger) {
+      alertLED.on();
+      amberLED.off();
+    } else {
+      alertLED.off();
+    }
+
+    if (isAmber) {
+      alertLED.off();
+      amberLED.on();
+    } else {
+      amberLED.off();
+    }
+
   });
 
   this.repl.inject({
